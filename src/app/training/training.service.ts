20f28bcde36285e76b6ba51f0899124c1b3ddd78
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2018-03-16 22:26:40 +0100

    + add startExercise to training service and use it from training component

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,9 @@
 import { Exercise } from './exercise.model';
+import { Subject } from 'rxjs/Subject';
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -8,7 +11,14 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
 
+  private runningExercise: Exercise;
+
   getAvailableExercises() {
     return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({ ...this.runningExercise });
   }
 }
